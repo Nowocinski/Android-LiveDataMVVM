870aaedde8c6a68eb8f1ee3be07b1f0455535c0e
Nowocinski <vito24222@gmail.com>
2022-06-04 14:21:28 +0200

    Added fragment display

--- app/src/main/java/com/example/livedatamvvm/MainActivity.java
+++ app/src/main/java/com/example/livedatamvvm/MainActivity.java
@@ -10,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment1 fragment1 = new Fragment1();
+        Fragment2 fragment2 = new Fragment2();
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainer1, fragment1)
+                .replace(R.id.fragmentContainer2, fragment2)
+                .commit();
     }
 }
