19685fe83a867aced27cd54019384d9d796e3365
Nowocinski <vito24222@gmail.com>
2022-06-04 15:01:13 +0200

    Added send text to ViewModel

--- app/src/main/java/com/example/livedatamvvm/Fragment1.java
+++ app/src/main/java/com/example/livedatamvvm/Fragment1.java
@@ -2,16 +2,53 @@ package com.example.livedatamvvm;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.livedatamvvm.databinding.Fragment1Binding;
+
 public class Fragment1 extends Fragment {
+    private FragmentViewModel viewModel;
+    private Fragment1Binding binding;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_1, container, false);
+        this.binding = Fragment1Binding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        return root;
+        //return inflater.inflate(R.layout.fragment_1, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        this.viewModel = new ViewModelProvider(this, (ViewModelProvider.Factory) new ViewModelProvider.NewInstanceFactory())
+                .get(FragmentViewModel.class);
+        this.binding.editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                viewModel.setString(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
